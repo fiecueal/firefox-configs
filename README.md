c589455d98fc59f8a9b0a0684c7bcff0a3bb384d
fiecueal <fiecueal@proton.me>
2024-10-17 22:20:23 +0800

    user Betterfox for user.js

--- README.md
+++ README.md
@@ -4,12 +4,23 @@
 - puts url & tab bars in the same row
 - removes margins/padding/borders in various places
 
-`user.js` changes miscellaneous settings which are commented in the file itself
+`user.js` are extra configs to slap under [Betterfox](https://github.com/yokoffing/Betterfox) `user.js`.
+Found Betterfox just after making this repo and decided to use that instead of making a `user.js` from scratch.
 
 ## Usage
 
-For an existing profile, use this command to copy the files.
-If they already exist in the profile, this will back them up.
-`cp -r --backup=numbered user.js chrome/ <profile directory here>`
+Install Betterfox then do:
 
-To make a new one, just run the shell script.
+```sh
+# new profile
+name="new profile name here"
+firefox -CreateProfile "$name"
+sleep 2 # give firefox time to create the new profile
+dir=$(find $HOME/.mozilla/firefox/ -maxdepth 1 -name "*.$name")
+# new profile
+
+# assumes Betterfox repo exists in $HOME/Projects/Betterfox
+cp -r --backup=numbered chrome/ $HOME/Projects/Betterfox/user.js $dir
+# append personal configs to the user.js that is now in the profile dir
+cat user.js >> $dir/user.js
+```
--- user.js
+++ user.js
@@ -1,47 +1,27 @@
-// prevent backspace from going back a page
-user_prefs("browser.backspace_action", 1);
-// disable search suggestions
-user_prefs("browser.search.suggest.enabled", false);
-// open firefox with previous session tabs
-user_prefs("browser.startup.page", 3);
-// don't immediately kill the window with ctrl+q/window close
+/*** slap this at the bottom of betterfox user.js ***/
+
+user_pref("browser.backspace_action", 1);
+// open with previous session tabs
+user_pref("browser.startup.page", 3);
 user_pref("browser.tabs.hoverPreview.showThumbnails", false);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-// reactive compact ui mode
-user_pref("browser.compactmode.show", true);
+// activate compact ui mode
 user_pref("browser.uidensity", 1);
 // always ask where to download files
 user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.download.useDownloadDir", false);
 // open bookmarks in new tab
 user_pref("browser.tabs.loadBookmarksInTabs", true);
-// allows chrome/userChrome.css to be loaded by firefox
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // disable drm content
 user_pref("media.eme.enabled", false);
 // disable firefox accounts
 user_pref("identity.fxaccounts.enabled", false);
-// privacy/security
-user_pref("browser.safebrowsing.malware.enabled", true);
-user_pref("browser.safebrowsing.phishing.enabled", true);
 user_pref("privacy.donottrackheader.enabled", true);
-user_pref("privacy.globalprivacycontrol.enabled", true);
-user_pref("privacy.globalprivacycontrol.functionality.enabled", true);
 user_pref("signon.autofillForms", false);
-// disable telemetry/studies/sponsors (probably incomplete)
-user_pref("app.shield.optoutstudies.enabled", false);
+// telemetry/sponsors
 user_pref("browser.ping-centre.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
-user_pref("browser.newtabpage.activity-stream.telemetry", false);
-user_pref("browser.urlbar.suggest.pocket", false);
-user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("toolkit.telemetry.bhrPing.enabled", false);
 user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
-// new tab page stuff
+// new tab page
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
-user_pref("browser.newtabpage.activity-stream.showWeather", false);
